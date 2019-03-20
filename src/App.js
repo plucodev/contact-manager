5cadbe363ed596dbe35db6ba4ff8c5a1356b4ada
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-03-20 01:12:32 +0000

    Started setting up Header and installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header branding="Contact Manager"/>
+        <Contact name="Paolo Lucano" email="pluco@gmail.com" phone="555-555-5555"/>
+        <Contact name="Jennifer Lucano" email="jennifer@gmail.com" phone="667-555-5555"/>
       </div>
     );
   }
